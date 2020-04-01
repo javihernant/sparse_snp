9c66952346f8e4752036f338d6510339b2800e7a
Miguel Angel Martinez del Amor <mdelamor@us.es>
2020-04-01 19:41:02 +0200

    First initialization code

--- include/sparse_snp.h
+++ include/sparse_snp.h
@@ -11,18 +11,50 @@
 #define NO_DEBUG 0
 #define DEBUG    1
 
+typedef unsigned short int ushort;
+typedef unsigned int uint;
+typedef unsigned char uchar;
 
-typedef struct
-{
-// params of the simulation (CPU, GPU, ...)
+// typedef struct
+// {
+// // params of the simulation (CPU, GPU, ...)
 	
-} SNP_PARAMS;
+// } SNP_PARAMS;
 
 
 typedef struct
 {
-// rules, synapses, neurons
+    uint n;                   // number of neurons
+    uint m;                   // number of rules
+
+    // CPU part
+    ushort * conf_vector;     // configuration vector (# neurons)
+    short  * trans_matrix;    // transition matrix (# rules * # neurons), requires negative numbers
+    ushort * spiking_vector;  // spiking vector (# neurons)
+    uint   * rule_index;      // indicates for each neuron, the starting rule index (# neurons+1)
+
+    struct _rule {
+        uchar  * En;
+        uchar  * Ei;
+        uchar  * c;    
+        uchar  * p;
+    } rules, d_rules;
+
+    // GPU counterpart
+    ushort * d_conf_vector;
+    uchar  * d_trans_matrix;
+    ushort * d_spiking_vector;
+    uint   * drule_index;      // indicates for each neuron, the starting rule index (# neurons+1)
+    
 } SNP_STRUCT;
+
+
+
+
+
+
+
+
 
 
 
--- src/sparse_snp.c
+++ src/sparse_snp.c
@@ -12,9 +12,14 @@ void init_params()
 }
 
 
-void init_vars()
+void init_vars(SNP_STRUCT **snp, int n, int m)
 {
-	
+    *snp = (SNP_STRUCT*) malloc(sizeof(SNP_STRUCT*));
+    (*snp)->n = n;
+    (*snp)->m = m;
+    (*snp)->conf_vector = malloc(sizeof(ushort)*n);
+    (*snp)->spiking_vector = malloc(sizeof(ushort)*m);
+    (*snp)->trans_matrix = malloc(sizeof(ushort)*n*m);
 }
 
 void free_memory()
